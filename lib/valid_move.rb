e0d6d87b837cd4e3052d37c6c7a163ce614e27a6
Michael Carroll <carrollm2@gmail.com>
2019-03-01 02:45:09 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,10 +2,12 @@
 def valid_move(board, index)
   if index between?(0, 8)
     if position_taken?
-
+      return true
     else
-
+      return false
     end
+  else
+    return false  
   end
 end
 
